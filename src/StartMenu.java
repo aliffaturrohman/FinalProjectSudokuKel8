fb39c84e92012fcbbb9b9fe2cea58317dfd40eb1
Alif Faturrohman <aliffaturrohman11@gmail.com>
2023-12-06 17:42:25 +0700

    Update start page

--- src/StartMenu.java
+++ src/StartMenu.java
@@ -1,6 +1,11 @@
+import sudoku.SudokuMain;
+import tictactoe.TTTGraphics;
+
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import javax.imageio.*;
@@ -12,12 +17,16 @@ public class StartMenu extends JFrame {
         int buttonWidth = 220;
         int buttonHeight = 50;
 
+
+
+
         JFrame frame = new JFrame("Start Menu"); // Set the title for the JFrame
         JButton buttonSudoku = new JButton("Sudoku");
         JButton buttonTicTacToe = new JButton("TicTacToe");
+        ImageIcon icon = new ImageIcon("logo.png");
+        frame.setIconImage(icon.getImage());
         buttonSudoku.setBounds((width * 3 / 4) - (buttonWidth/2), (height / 2) - (buttonHeight/2), buttonWidth, buttonHeight);
         buttonTicTacToe.setBounds((width / 4) - (buttonWidth/2), (height / 2) - (buttonHeight/2), buttonWidth, buttonHeight);
-
         buttonSudoku.setBorder(new RoundedBorder(50));
         buttonTicTacToe.setBorder(new RoundedBorder(50));
         buttonSudoku.setForeground(Color.RED);
@@ -29,8 +38,30 @@ public class StartMenu extends JFrame {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Set default close operation
         frame.setLayout(null); // Using no layout manager
         frame.setVisible(true); // Making the frame visible
+
+        buttonSudoku.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                        new SudokuMain();
+                    }
+                });
+            }
+        });
+        buttonTicTacToe.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                        new TTTGraphics();
+                    }
+                });
+            }
+        });
     }
+
+
 }
+
 
 class RoundedBorder implements Border {
     private int radius;
