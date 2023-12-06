f363363308673f8c641e9fcbbd6f7f5c3ba61346
Alif Faturrohman <aliffaturrohman11@gmail.com>
2023-12-06 17:10:54 +0700

    Add start page for two games

--- src/StartMenu.java
+++ src/StartMenu.java
@@ -1,2 +1,49 @@
-public class StartMenu {
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.*;
+
+public class StartMenu extends JFrame {
+    public static void main(String[] args) {
+        int width = 800;
+        int height = 600;
+        int buttonWidth = 220;
+        int buttonHeight = 50;
+
+        JFrame frame = new JFrame("Start Menu"); // Set the title for the JFrame
+        JButton buttonSudoku = new JButton("Sudoku");
+        JButton buttonTicTacToe = new JButton("TicTacToe");
+        buttonSudoku.setBounds((width * 3 / 4) - (buttonWidth/2), (height / 2) - (buttonHeight/2), buttonWidth, buttonHeight);
+        buttonTicTacToe.setBounds((width / 4) - (buttonWidth/2), (height / 2) - (buttonHeight/2), buttonWidth, buttonHeight);
+
+        buttonSudoku.setBorder(new RoundedBorder(50));
+        buttonTicTacToe.setBorder(new RoundedBorder(50));
+        buttonSudoku.setForeground(Color.RED);
+        buttonTicTacToe.setForeground(Color.RED);
+        frame.add(buttonSudoku); // Adding button to JFrame
+        frame.add(buttonTicTacToe); // Adding button to JFrame
+
+        frame.setSize(width, height); // Set the size of the frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Set default close operation
+        frame.setLayout(null); // Using no layout manager
+        frame.setVisible(true); // Making the frame visible
+    }
+}
+
+class RoundedBorder implements Border {
+    private int radius;
+    RoundedBorder(int radius) {
+        this.radius = radius;
+    }
+    public Insets getBorderInsets(Component c) {
+        return new Insets(this.radius + 1, this.radius + 1, this.radius + 2, this.radius);
+    }
+    public boolean isBorderOpaque() {
+        return true;
+    }
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        g.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+    }
 }
