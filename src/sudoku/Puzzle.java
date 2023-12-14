2c67ec00e505e5a2f813904cb4e7b6cb542f00ee
Alif Faturrohman <aliffaturrohman11@gmail.com>
2023-12-14 13:11:55 +0700

    add prevention against input of characters

--- src/sudoku/Puzzle.java
+++ src/sudoku/Puzzle.java
@@ -1,6 +1,5 @@
 package sudoku;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class Puzzle {
--- src/sudoku/SudokuMain.java
+++ src/sudoku/SudokuMain.java
@@ -1,11 +1,7 @@
 package sudoku;
 import java.awt.*;
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 /**
  * The main Sudoku program
